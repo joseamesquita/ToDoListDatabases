f4caf03a7dedcad127d4d372015ccb5217e0da34
Jose Amesquita <joseamesquita11@gmail.com>
2020-03-16 14:20:40 -0700

    configure database connection

---
+++ ToDoList/Startup.cs
@@ -0,0 +1,12 @@
+namespace ToDoList
+{
+  public class Startup
+  {
+
+  }
+
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
+  }
+}
